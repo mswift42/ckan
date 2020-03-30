420390d434773610ab402e665c7da07a8287761b
mswift42 <martin.haesler@gmail.com>
2020-03-30 08:07:44 +0100

    add file last_search_service.dart.

--- lib/last_search_service.dart
+++ lib/last_search_service.dart
@@ -1,7 +1,7 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
-import 'dart:async';
 
 class LastSearchService {
   final String lastSearchesFileCK = 'lastsearches.txt';
