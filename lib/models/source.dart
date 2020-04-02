de6fdec48a1230031fc9747e2441ed809d68e2bc
mswift42 <martin.haesler@gmail.com>
2020-04-02 07:53:46 +0100

    fix source class.

--- lib/models/source.dart
+++ lib/models/source.dart
@@ -1,7 +1,7 @@
 import 'package:ckan/recipe_service.dart';
 import 'package:flutter/material.dart';
 
-class Source extends ChangeNotifier {
+class Source {
   static const _sources = [
     RecipeSource('Chefkoch'),
     RecipeSource('BBCGF'),
