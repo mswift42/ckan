87b56ec2575ac3d5fed218fadef246cd85d936f6
mswift42 <martin.haesler@gmail.com>
2020-04-03 08:18:47 +0100

    add field and getter/setter for activeSource.

--- lib/models/source.dart
+++ lib/models/source.dart
@@ -1,7 +1,17 @@
 import 'package:ckan/recipe_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class Source {
+class Source extends ChangeNotifier {
+  RecipeSource _source;
+
+  RecipeSource get source => _source;
+
+  set source(RecipeSource newSource) {
+    _source = newSource;
+    notifyListeners();
+  }
+
   static const _sources = [
     RecipeSource('Chefkoch'),
     RecipeSource('BBCGF'),
