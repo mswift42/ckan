c8f411f9368dee1a9c5bc9c4670564c0182c20bb
mswift42 <martin.haesler@gmail.com>
2020-04-13 08:48:03 +0100

    add field activeSource.

--- lib/models/lastsearch.dart
+++ lib/models/lastsearch.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:ckan/models/source.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
@@ -8,29 +9,30 @@ import 'package:path_provider/path_provider.dart';
 // TODO Save and restore from both sources separately.
 
 class LastSearchModel extends ChangeNotifier {
+  final SourceName activeSoruce = SourceName.chefkoch;
   final String lastSearchesFileCK = 'lastsearches.txt';
   final String lastSearchesFileBBCGF = 'lastSearchesbbcgf.txt';
   Set<String> _savedSearches = {};
 
   LastSearchModel() {
-    lastSearches();
+    lastSearches(activeSoruce);
   }
 
-  Future<void> lastSearches() async {
-    readSearches().then((value) => _savedSearches = value.toSet());
+  Future<void> lastSearches(SourceName source) async {
+    readSearches(source).then((value) => _savedSearches = value.toSet());
   }
 
   Set<String> get searches => _savedSearches;
 
-  void add(String search) {
+  void add(String search, SourceName source) {
     _savedSearches.add(search);
-    writeSearches(_savedSearches.toList());
+    writeSearches(_savedSearches.toList(), source);
     notifyListeners();
   }
 
   void remove(String search) {
     _savedSearches.remove(search);
-    writeSearches(_savedSearches.toList());
+    writeSearches(_savedSearches.toList(), activeSoruce);
     notifyListeners();
   }
 
@@ -39,22 +41,41 @@ class LastSearchModel extends ChangeNotifier {
     return directory.path;
   }
 
-  Future<File> get _localFile async {
+  Future<File> get _localFileCK async {
     final path = await _localPath;
     return File('$path/$lastSearchesFileCK');
   }
 
-  Future<File> writeSearches(List<String> searches) async {
-    final file = await _localFile;
-    return file.writeAsString(searches.join(','));
+  Future<File> get _localFileBBCGF async {
+    final path = await _localPath;
+    return File('$path/$lastSearchesFileBBCGF');
   }
 
-  Future<List<String>> readSearches() async {
-    try {
-      final file = await _localFile;
+  Future<File> writeSearches(List<String> searches, SourceName source) async {
+    switch (source) {
+      case SourceName.bbcgf:
+        final file = await _localFileBBCGF;
+        return file.writeAsString(searches.join(','));
+        break;
+      default:
+        final file = await _localFileCK;
+        return file.writeAsString(searches.join(','));
+    }
+  }
 
-      String contents = await file.readAsString();
-      return contents.split(',');
+  Future<List<String>> readSearches(SourceName source) async {
+    try {
+      switch (source) {
+        case SourceName.bbcgf:
+          final file = await _localFileBBCGF;
+          String contents = await file.readAsString();
+          return contents?.split(',') ?? [];
+          break;
+        default:
+          final file = await _localFileCK;
+          String contents = await file.readAsString();
+          return contents?.split(',') ?? [];
+      }
     } catch (e) {
       print(e);
     }
